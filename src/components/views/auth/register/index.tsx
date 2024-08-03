5383ac5ecde771667affcc3a517f48b44970228c
ardigiandi <ardiansyahg9080@gmail.com>
2024-08-04 00:44:42 +0700

    add : registerviews

---
+++ src/components/views/auth/register/index.tsx
@@ -0,0 +1,18 @@
+const RegisterViews = () => {
+    return(
+        <div>
+            <h1>Register</h1>
+            <div>
+                <form action="">
+                    <div>
+                        <label htmlFor=""></label>
+                        <input type="text" />
+                    </div>
+                    <button>Register</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterViews
---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,9 @@
+import RegisterViews from "@/components/views/auth/register"
+
+const Register = () => {
+    return(
+        <RegisterViews/>
+    )
+}
+
+export default Register
